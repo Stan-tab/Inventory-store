cd7894490fc77fa3c5b4ba2a110d8270c7253aae
Stan <75632684+Stan-tab@users.noreply.github.com>
2025-08-23 01:26:08 -0700

    Add some styles and path links

--- routers/indexRoute.js
+++ routers/indexRoute.js
@@ -7,6 +7,9 @@ indexRouter.get("/", mainController.indexPageGet);
 indexRouter.get("/search", (req, res) => {
 	console.log(req.query.q);
 });
+indexRouter.post("/searchBy", (req, res) => {
+	console.log(req.body);
+});
 indexRouter.all("/{*splat}", mainController.E404);
 
 module.exports = indexRouter;
